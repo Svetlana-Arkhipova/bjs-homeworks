3d49831c39a9513270a7b2918e054f1395846130
Svetlana Arkhipova <sve.tochka@inbox.ru>
2020-05-15 13:13:50 +0300

    Решение ДЗ-1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,40 @@
+'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    let values = [percent, contribution, amount];
+    let types = ['percent', 'contribution', 'amount'];
+
+    for (let i = 0; i < values.length;) {
+      if (typeof(values[i]) === 'number' && isNaN(values[i]) === false) {
+        i++;
+      } else {
+        values[i] = parseInt(values[i]);
+        if (typeof(values[i]) === 'number' && isNaN(values[i]) === false) {
+          i++;
+        } else {
+          return(`Параметр ${types[i]} содержит неправильное значение ${values[i]}`);
+        }
+      }
+    }
+
+    let s = amount - contribution;
+
+    //кол-во месяцев = кол-во месяцев за полные годы
+    //плюс оставшиеся месяцы текущего года (не включая текущий месяц)
+    //плюс месяцы последнего года выплат
+    let months = (date.getFullYear() - new Date().getFullYear() - 1) * 12 + (11 - new Date().getMonth()) + (date.getMonth() + 1);
+    let p = percent / 100 / 12;
+    let monthlyFee = s * (p + p / (((1 + p) ** months) - 1));
+    let totalAmount = monthlyFee * months;
+    console.log(totalAmount.toFixed(2));
+    return (totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    (name && name != 'null' && name != 'undefined' && name != ' ') ?
+      greeting = `Привет, мир! Меня зовут ${name}` : greeting = `Привет, мир! Меня зовут ${'Аноним'}`
+    console.log(greeting);
+    return greeting;
 }
