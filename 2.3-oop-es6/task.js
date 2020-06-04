8c35ef5c22dca708cd662eb64035295ab9add788
Svetlana Arkhipova <sve.tochka@inbox.ru>
2020-06-04 17:21:08 +0300

    Решение ДЗ 2.3, задача 2

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -95,3 +95,88 @@ longBow.takeDamage(100);
 console.log(longBow.durability);
 console.log(longBow.getDamage());
 console.log(longBow.isBroken());
+
+
+//Задача 2
+class Arm extends Weapon {
+  constructor() {
+    super({
+      name: 'Рука',
+      attack: 1,
+      durability: Infinity,
+      range: 1,
+    })
+  }
+}
+
+class Bow extends Weapon {
+  constructor() {
+    super({
+      name: 'Лук',
+      attack: 10,
+      durability: 200,
+      range: 3,
+    })
+  }
+}
+
+class LongBow extends Bow {
+  constructor() {
+    super()
+    this.name = 'Длинный лук';
+    this.attack = 15;
+    this.range = 4;
+    }
+  }
+
+
+class Sword extends Weapon {
+  constructor() {
+    super({
+      name: 'Меч',
+      attack: 25,
+      durability: 500,
+      range: 1,
+    })
+  }
+}
+
+class Axe extends Sword {
+  constructor() {
+    super()
+    this.name = 'Секира';
+    this.attack = 27;
+    this.durability = 800;
+    }
+  }
+
+  class Knife extends Weapon {
+    constructor() {
+      super({
+        name: 'Нож',
+        attack: 5,
+        durability: 300,
+        range: 1,
+      })
+    }
+  }
+
+  class Staff extends Weapon {
+    constructor() {
+      super({
+        name: 'Посох',
+        attack: 8,
+        durability: 300,
+        range: 2,
+      })
+    }
+  }
+
+  class StormStaff extends Staff {
+    constructor() {
+      super()
+      this.name = 'Посох Бури';
+      this.attack = 10;
+      this.range = 3;
+      }
+    }
